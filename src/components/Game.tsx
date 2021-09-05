9c067ab450c15d434cc8f599f9ddd3c9bd053c91
ytsujiii <tsujimon850734@gmail.com>
2021-09-06 00:19:10 +0900

    feat: キーボード入力の詳細な制御

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,18 +1,24 @@
-import React, { useCallback, useEffect, useReducer } from 'react';
+import React, { useCallback, useEffect, useReducer, useRef, useState } from 'react';
 import useMinoReducer from '../hooks/useMinoReducer';
 import BoardComponent from './Board';
 
 export default function Game(): React.ReactElement {
+  const das = 100; // (ms)
   const [shouldClearTimer, timerClearer] = useReducer((state: number) => state + 1, 0);
   const [boardState, boardUpdater] = useReducer((state: number) => state + 1, 0);
+  const [dropping, setDropping] = useState<boolean>(false);
+  const [movingLeft, setMovingLeft] = useState<boolean>(false);
+  const [movingRight, setMovingRight] = useState<boolean>(false);
+  const timeoutIdRef = useRef<number>();
+  const intervalIdRef = useRef<number>();
   const [squaresRef, moveLeft, moveRight, drop, hardDrop, rotateLeft, rotateRight] = useMinoReducer(
     boardUpdater,
     timerClearer
   );
 
-  const handleKeyPress = useCallback(
-    (ev: KeyboardEvent) => {
-      console.log(ev.key);
+  const handleKeyDown = useCallback(
+    async (ev) => {
+      if (ev.repeat) return;
       switch (ev.key) {
         case 'z':
           rotateLeft();
@@ -20,35 +26,88 @@ export default function Game(): React.ReactElement {
         case 'x':
           rotateRight();
           break;
-        case 'ArrowLeft':
-          moveLeft();
-          break;
-        case 'ArrowRight':
-          moveRight();
-          break;
         case 'ArrowUp':
           hardDrop();
+          break;
+        case 'Space': // hold
+          break;
+        case 'ArrowDown':
+          setDropping(true);
+          break;
+        case 'ArrowLeft':
+          setMovingLeft(true);
+          break;
+        case 'ArrowRight':
+          setMovingRight(true);
           break;
         default:
           break;
       }
     },
-    [rotateLeft, rotateRight, moveLeft, moveRight]
+    [rotateLeft, rotateRight, hardDrop]
   );
+  const handleKeyUp = useCallback((ev) => {
+    switch (ev.key) {
+      case 'ArrowDown':
+        setDropping(false);
+        break;
+      case 'ArrowLeft':
+        setMovingLeft(false);
+        break;
+      case 'ArrowRight':
+        setMovingRight(false);
+        break;
+      default:
+        break;
+    }
+  }, []);
 
   useEffect(() => {
+    if (!dropping) return;
+    const id = setInterval(() => {
+      drop();
+    }, 50);
+    return () => clearInterval(id);
+  }, [dropping]);
+  useEffect(() => {
+    if (!movingLeft) return;
+    moveLeft();
+    timeoutIdRef.current = window.setTimeout(() => {
+      intervalIdRef.current = window.setInterval(() => moveLeft(), 25);
+    }, das);
+    return () => {
+      window.clearTimeout(timeoutIdRef.current);
+      window.clearInterval(intervalIdRef.current);
+    };
+  }, [movingLeft]);
+  useEffect(() => {
+    if (!movingRight) return;
+    moveRight();
+    timeoutIdRef.current = window.setTimeout(() => {
+      intervalIdRef.current = window.setInterval(() => moveRight(), 25);
+    }, das);
+    return () => {
+      window.clearTimeout(timeoutIdRef.current);
+      window.clearInterval(intervalIdRef.current);
+    };
+  }, [movingRight]);
+
+  useEffect(() => {
+    if (dropping) return;
     const id = setInterval(() => {
       console.debug('time elapsed');
       drop();
     }, 1000);
     return () => clearInterval(id);
-  }, [drop, shouldClearTimer]);
+  }, [drop, shouldClearTimer, dropping]);
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
     };
-  }, [handleKeyPress]);
+  }, [handleKeyDown, handleKeyUp]);
 
   return <>{!squaresRef.current || <BoardComponent boardState={boardState} squares={squaresRef.current} />}</>;
 }
