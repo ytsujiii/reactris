4a776baad240292a69c7057f16a7e934006dc98a
ytsujiii <tsujimon850734@gmail.com>
2021-09-06 23:25:04 +0900

    style: bodyのマージンを除去

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Game from './components/Game';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
