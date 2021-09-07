0d1ca0e18c050a7a73c2e81b7fdfb54e885c30dc
ytsujiii <tsujimon850734@gmail.com>
2021-09-07 22:30:11 +0900

    style: MinoBagComponent

--- src/components/MinoBagComponent.tsx
+++ src/components/MinoBagComponent.tsx
@@ -9,11 +9,29 @@ interface Props {
 }
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    border: '1px solid white',
+    marginLeft: 10,
+    width: 120,
+    height: 400,
+    padding: '30px 0',
+  },
+  label: {
+    position: 'absolute',
+    top: 35,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 21,
+    backgroundColor: 'black',
+    padding: '0 5px 0 7px',
+    letterSpacing: 2,
+  },
   row: {
     display: 'flex',
-  },
-  next: {
-    margin: '25px 0',
   },
 });
 
@@ -26,9 +44,10 @@ export default function MinoBagComponent(props: Props): React.ReactElement {
   } else {
     const nexts = [...minoBag, ...nextMinoBag].slice(0, 5).map((minoType) => getFittedMinoShape(minoType, 0));
     return (
-      <div>
+      <div className={classes.root}>
+        <div className={classes.label}>NEXT</div>
         {nexts.map((minoShape, i) => (
-          <div key={i} className={classes.next}>
+          <div key={i}>
             {minoShape.map((row, y) => (
               <div className={classes.row} key={y}>
                 {row.map((square, x) => (
