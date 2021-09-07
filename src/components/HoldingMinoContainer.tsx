44ecf3c0fda5d3739629ae552aa19216f8571894
ytsujiii <tsujimon850734@gmail.com>
2021-09-07 22:22:22 +0900

    style: HoldingMinoContainer

--- src/components/HoldingMinoContainer.tsx
+++ src/components/HoldingMinoContainer.tsx
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
-import { getMinoShape, MinoInterface } from '../constants/Mino';
+import { BlockType, getFittedMinoShape, MinoInterface } from '../constants/Mino';
 import Square from './Square';
 
 interface Props {
@@ -8,8 +8,29 @@ interface Props {
 }
 
 const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid white',
+    height: 100,
+    width: 120,
+    overflow: 'visible',
+    marginRight: 10,
+  },
   row: {
     display: 'flex',
+  },
+  label: {
+    position: 'absolute',
+    top: 35,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 21,
+    backgroundColor: 'black',
+    padding: '0 5px 0 7px',
+    letterSpacing: 2,
   },
 });
 
@@ -17,21 +38,18 @@ export default function HoldingMinoContainer(props: Props): React.ReactElement {
   const { holdingMino } = props;
   const classes = useStyles();
 
-  if (holdingMino) {
-    const minoShape = getMinoShape(holdingMino.type, holdingMino.rotation);
-    return (
-      <div>
-        {!minoShape ||
-          minoShape.map((row, y) => (
-            <div className={classes.row} key={y}>
-              {row.map((square, x) => (
-                <Square state={square} key={x} />
-              ))}
-            </div>
-          ))}
-      </div>
-    );
-  } else {
-    return <></>;
-  }
+  const minoShape = holdingMino ? getFittedMinoShape(holdingMino.type, holdingMino.rotation) : [[BlockType.none]];
+  return (
+    <div className={classes.root}>
+      <div className={classes.label}>HOLD</div>
+      {!minoShape ||
+        minoShape.map((row, y) => (
+          <div className={classes.row} key={y}>
+            {row.map((square, x) => (
+              <Square state={square} key={x} />
+            ))}
+          </div>
+        ))}
+    </div>
+  );
 }
